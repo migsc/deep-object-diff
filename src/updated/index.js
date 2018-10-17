61d2bd83078f1890bdf1391974e606954b3e3f2b
Miguel Chateloin <mchateloin@cenergistic.com>
2018-10-17 15:31:41 -0500

    see the previous value in an update

--- src/updated/index.js
+++ src/updated/index.js
@@ -1,25 +1,31 @@
-import { isDate, isEmpty, isObject, properObject } from '../utils';
+import { isDate, isEmpty, isObject, properObject } from "../utils";
 
 const updatedDiff = (lhs, rhs) => {
-
   if (lhs === rhs) return {};
 
-  if (!isObject(lhs) || !isObject(rhs)) return rhs;
+  if (!isObject(lhs) || !isObject(rhs))
+    return `${lhs
+      .toString()
+      .split("")
+      .join("\u0336")} ${rhs.toString()}`;
 
   const l = properObject(lhs);
   const r = properObject(rhs);
 
   if (isDate(l) || isDate(r)) {
     if (l.valueOf() == r.valueOf()) return {};
-    return r;
+    return `${l
+      .toISOString()
+      .split("")
+      .join("\u0336")} ${r.toISOString()}`;
   }
 
   return Object.keys(r).reduce((acc, key) => {
-
     if (l.hasOwnProperty(key)) {
       const difference = updatedDiff(l[key], r[key]);
 
-      if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc;
+      if (isObject(difference) && isEmpty(difference) && !isDate(difference))
+        return acc;
 
       return { ...acc, [key]: difference };
     }
